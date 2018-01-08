ed02c2cb91416b4afe92e054f2a8f91a2b9ebaf2
Krishna <srikrishnats@langoor.com>
2018-01-09 02:11:42 +0530

    cab test cases

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,43 @@
-class Ride < ApplicationRecord
+class Ride < ActiveRecord::Base
+  include Location
+  attr_accessor :conditions
+
+  belongs_to :cab
+
+  validates :cab, presence: true
+  validates :start_latitude, presence: true
+  validates :start_longitude, presence: true
+  validates :end_latitude, presence: true, if: Proc.new { |cab| cab.ended? }
+  validates :end_longitude, presence: true, if: Proc.new { |cab| cab.ended? }
+
+  enum status: [:unstarted, :started, :ended]
+
+  before_validation :assign_empty_cab, if: :new_record?
+
+  after_initialize :set_state
+
+
+  def start
+    @state.start
+  end
+
+  def end(destination)
+    @state.end(destination)
+  end
+
+  def payment_amount
+    @state.payment_amount
+  end
+
+
+  private
+
+  def set_state
+    @state = RideStateFactory.create(self)
+  end
+
+  def assign_empty_cab
+    self.cab = EmptyCab.nearest_to(start_latitude, start_longitude, conditions)
+    self.cab.assign if self.cab
+  end
 end
--- test/models/cab_test.rb
+++ test/models/cab_test.rb
@@ -4,4 +4,15 @@ class CabTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "Should be invalid without latitude" do
+      cab = Cab.new
+      assert_not cab.save, "Saved a cab without latitude"
+  end
+
+  test "Should be invalid without longitude" do
+      cab = Cab.new
+      assert_not cab.save, "Saved a cab without longitude"
+  end
+
 end
